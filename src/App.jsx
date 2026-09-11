f182d7ef88279cccc0563032a2a84dac80bfe857
Eetu Lämsä <lamsaeetu01@gmail.com>
2026-09-11 09:18:21 +0300

    begin of home and login screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,21 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+import { Routes, Route, Link } from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home'
+import Login from './pages/login'
 
 function App() {
-  const [data, setData] = useState([])
-  const [loading, setLoading] = useState(true)
-  const [error, setError] = useState(null)
-
-  const fetchData = async () => {
-    setLoading(true)
-    setError(null)
-    
-    try {
-      const response = await axios.get(`${import.meta.env.VITE_API_URL}/`)
-      setData(response.data)
-    } catch (err) {
-      setError(err.message)
-    } finally {
-      setLoading(false)
-    }
-  }
-
-  useEffect(() => {
-    fetchData()
-  }, [])
-
   return (
     <>
-      <h1>Docker Sample</h1>
-      <div className="card">
-        {loading && <p>Loading...</p>}
-        
-        {data && (
-          <div style={{ marginTop: '20px', padding: '10px', background: '#f0f0f0' }}>
-            <h3>Data from Backend:</h3>
-            <ul style={{ textAlign: 'left', margin: '10px 0' }}>
-              { data.map((item) => (
-                <li key={item.id} style={{ marginBottom: '10px' }}>
-                  {`ID: ${item.id} - ${item.description}`}
-                </li>
-              ))}
-            </ul>
-          </div>
-        )}
-        
-        {error && (
-          <div style={{ marginTop: '20px', padding: '10px', background: '#ffebee', color: 'red' }}>
-            <h3>Error:</h3>
-            <p>{error}</p>
-          </div>
-        )}
-      </div>
+      <nav>
+        <Link to="/">Etusivu</Link>
+        {' | '}
+        <Link to="/login">Kirjaudu</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </>
   )
 }
